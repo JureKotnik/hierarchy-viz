5d6a65ecb55ab24728937e06f2ab90b9ebb8a3ed
JureKotnik <jure2002k@gmail.com>
2026-02-03 18:28:54 +0100

    feature: implement recursive search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NodeComponent } from "./components/node/node.component";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { NodeComponent } from './components/node/node.component';
+import { TreeDataService } from './services/tree-data.service';
+import { TreeNode } from './models/tree-node.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NodeComponent],
+  standalone: true,
+  imports: [CommonModule, NodeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class App {
-  protected readonly title = signal('hierarchy-viz');
+export class AppComponent implements OnInit {
+  treeData$!: Observable<TreeNode[]>;
+
+  constructor(private treeService: TreeDataService) {}
+
+  ngOnInit(): void {
+    this.treeData$ = this.treeService.tree$;
+  }
+  onSearch(term: string): void {
+    this.treeService.search(term);
+  }
 }
